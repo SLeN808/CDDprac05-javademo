fde731ec8e4744f18bc84b27fbb724036c616994
Sidhardh Lenka <sidhardhlenka@gmail.com>
2021-10-05 16:44:38 +0530

    mul.java added

---
+++ Mul.java
@@ -0,0 +1,9 @@
+public class Mul  
+{  
+public static void main(String args[])   
+{  
+int n1 = 15, n2 = 7, mul;  
+mul = n1 * n2;  
+System.out.println("The product of numbers is: "+ mul);  
+}  
+}
