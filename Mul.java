8c23d01ae3a4d3c3cb34f517cbd0d6a7d39d814b
Sidhardh Lenka <sidhardhlenka@gmail.com>
2021-10-05 16:52:07 +0530

    tweaks

--- Mul.java
+++ Mul.java
@@ -5,5 +5,6 @@ public static void main(String args[])
 int n1 = 15, n2 = 7, mul;  
 mul = n1 * n2;  
 System.out.println("The product of numbers is: "+ mul);  
+System.out.println("SUCCESS");
 }  
 }
